6da1c2fa2d5242bfebb9965ac3e28854546e4ff6
Andrey Ryabinin <a.ryabinin@samsung.com>
2014-08-06 16:04:14 -0700

    mm: slab.h: wrap the whole file with guarding macro
    Guarding section:
    	#ifndef MM_SLAB_H
    	#define MM_SLAB_H
    	...
    	#endif
    currently doesn't cover the whole mm/slab.h. It seems like it was
    done unintentionally.
    
    Wrap the whole file by moving closing #endif to the end of it.
    
    Signed-off-by: Andrey Ryabinin <a.ryabinin@samsung.com>
    Acked-by: Christoph Lameter <cl@linux.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Reviewed-by: Vladimir Davydov <vdavydov@parallels.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- mm/slab.h
+++ mm/slab.h
@@ -229,7 +229,6 @@ static inline struct kmem_cache *cache_from_obj(struct kmem_cache *s, void *x)
 	WARN_ON_ONCE(1);
 	return s;
 }
-#endif
 
 
 /*
@@ -265,3 +264,5 @@ struct kmem_cache_node {
 
 void *slab_next(struct seq_file *m, void *p, loff_t *pos);
 void slab_stop(struct seq_file *m, void *p);
+
+#endif /* MM_SLAB_H */
